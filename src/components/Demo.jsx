7c2bb19fb5b140ebffc979b22377c0ae4d0223cf
Alina Rosenbusch <alina.rosenbusch@gmx.de>
2023-09-07 17:03:22 +0200

    add searchbar

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
-}
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          {/* Searchbar */}
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
 
-export default Demo
+          <input
+            type="url"
+            placeholder="Enter an URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-grey-700 peer-focus:text-grey-700"
+          >
+            ↵
+          </button>
+        </form>
+
+        {/* Browser URL History */}
+      </div>
+    </section>
+  );
+};
+
+export default Demo;
